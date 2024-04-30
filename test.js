4f69ba6566e5f364525e2440d7e27979ebf413c7
Eric Hoffman <eric.peter.hoffman@gmail.com>
2024-04-30 17:28:21 -0600

    Experimented with Types vs Interfaces

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
 console.log('Hello world!');
+// Defining types for function parameters and return type.
 const hello = (person, date) => {
-    let greetings = ['Hello', 'Howdy', 'Hola', 'Hallo', 'Yo'];
+    let greetings = ['Hello', 'Howdy', 'Hola', 'Hallo', 'Yo']; // Type of string[] refers to an array of strings. number[] refers to an array of numbers, etc.
     let numOfGreetings = greetings.length;
     return `${greetings[Math.floor(Math.random() * numOfGreetings)]} ${person}, today is ${date.toDateString()}!`;
 };
 console.log(hello('Brendan', new Date()));
+const printName = (name) => {
+    return `The name you provided is ${name.first} ${name.last}`;
+};
+console.log(printName({ first: 'Eric', last: 'Hoffman' }));
--- test.ts
+++ test.ts
@@ -1,10 +1,45 @@
 console.log('Hello world!');
 
+// Defining types for function parameters and return type.
 const hello = (person: string, date: Date): string => {
-  let greetings: string[] = ['Hello','Howdy','Hola','Hallo','Yo'];
+  let greetings: string[] = ['Hello','Howdy','Hola','Hallo','Yo']; // Type of string[] refers to an array of strings. number[] refers to an array of numbers, etc.
   let numOfGreetings: number = greetings.length;
 
   return `${greetings[Math.floor(Math.random() * numOfGreetings)]} ${person}, today is ${date.toDateString()}!`;
 };
 
 console.log(hello('Brendan', new Date()));
+
+
+type Name = { // Defining a custom, reusable type.
+  first: string,
+  last: string,
+}
+
+const printName = (name: Name) => { // Referencing custom type in function parameters.
+  return `The name you provided is ${name.first} ${name.last}`
+};
+
+console.log(printName({first: 'Eric', last: 'Hoffman'}));
+
+// Types vs Interfaces;
+
+// Here is an example of creating a type called Animal and another type called Bear.
+// Bear has the same properties as Animal, but in addition, has the honey property.
+// type Animal = {
+//   name: string,
+// }
+
+// type Bear = Animal & {
+//   honey: boolean,
+// }
+
+// Here is an example of creating an interface called Animal and another called Bear.
+// Bear is an extension of Animal, so it gets the properties of Animal, but it also has the honey property.
+// interface Animal {
+//   name: string,
+// }
+
+// interface Bear extends Animal {
+//   honey: boolean,
+// }
